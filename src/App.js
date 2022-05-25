4c9afffff51fb63053fd1768193a03aa841153c3
Darius Garcia <dariusgarcia888@gmail.com>
2022-05-25 00:17:59 -0700

    initialized Login component, and Landing component. Initizialized the routing.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
-import './App.css';
-import Login from './components/login';
+import "./App.css";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+
+// importing login component
+import Login from "./components/Login";
+//importing landing component
+import Landing from "./components/Landing";
 
 function App() {
   return (
-    <div className="App">
-     <Login />
-    </div>
+    <>
+      {/*This is the alias of BrowserRouter i.e. Router */}
+      <Router>
+        <Routes>
+          {/* This route is for Landing component 
+          with exact path "/", in component props 
+          we passes the imported component*/}
+          <Route exact path="/" component={Landing} />
+
+          {/* This route is for Login component 
+          with exact path "/Login", in component props 
+          we passes the imported component*/}
+          <Route exact path="/landing" component={Login} />
+          {/* If any route mismatches the upper route endpoints,
+         then redirect triggers and redirects app to landing component*/}
+          <Navigate to="/" />
+        </Routes>
+      </Router>
+    </>
+    // <div className="App">
+    //   <Login />
+    //   <Landing />
+    // </div>
   );
 }
 
--- src/components/login.js
+++ src/components/login.js
@@ -1,22 +1,55 @@
-import "./login.css"
+import "./Login.css";
+// import topo from '../topo.jpg';
 
 const Login = () => {
-    
+  return (
+    <div className="parent">
+      <h1>
+        {" "}
+        <span className="bull">Bull</span>
+        <span className="bear">Bear</span>
+      </h1>
+      <div className="login-container">
+        <form className="login-form">
+          <h5>Please sign in or register if you haven't!</h5>
+          <h2>Login</h2>
 
-    return (
-        <div>
-            <h1>BullBear</h1>
-            <form className="login-container">
-                <h2>Login</h2>
-                <label className="form heading" htmlFor="username" >Username</label>
-                <input type="text" name='name' placeholder="username" required /> 
-
-                <label className="form heading" htmlFor="password" >Password</label>
-                <input type="text" name='name' placeholder="password" required /> 
-            </form>
-        </div>
-        )
-}
-
+          <div className="label-box1">
+            <label className="form-heading" htmlFor="username">
+              <b>Username</b>
+            </label>
+            <input
+              className="input-email"
+              type="text"
+              name="name"
+              placeholder=" ex: JohnDoe34"
+              required
+            />
+          </div>
+          <div className="label-box2">
+            <label className="form-heading" htmlFor="password">
+              <b>Password</b>
+            </label>
+            <input
+              className="input-pw"
+              type="text"
+              name="name"
+              placeholder=" *******"
+              required
+            />
+          </div>
+          <div className="btn-container">
+            <button className="sign-btn" type="submit">
+              <b>SIGN IN</b>
+            </button>
+            <button className="up-btn" type="submit">
+              <b>SIGN UP</b>
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
 
 export default Login;
